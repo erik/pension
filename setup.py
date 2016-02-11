2d98cb68062f987cc34cd4d0602f567be46bd5a8
Erik Price <erikprice@openmail.co>
2016-02-11 10:25:05 -0800

    0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pension',
-    version='0.0.2',
+    version='0.0.3',
     description='Alert when ec2 instances are scheduled for retirement',
     author='Erik Price',
     url='https://github.com/erik/pension',
