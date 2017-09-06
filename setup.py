df3d4640305bdebef3953d0a98ab6e663524e575
Erik Price <github@erikprice.net>
2017-09-06 13:42:58 -0700

    0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pension',
-    version='0.0.8',
+    version='0.1.0',
     description='Alert when ec2 instances are scheduled for retirement',
     author='Erik Price',
     url='https://github.com/erik/pension',
